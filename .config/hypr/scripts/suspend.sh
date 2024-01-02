fd9a505eea154714c3c2bb5681ae3d84e5f874ea
Doug G <douglaswgerhardt@gmail.com>
2024-01-02 16:42:01 -0500

    Got media keys working

--- .config/hypr/scripts/suspend.sh
+++ .config/hypr/scripts/suspend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 swayidle -w \
-timeout 120 'notify-send "Idle for 2 minutes... locking" && sleep 5 && swaylock' \
-timeout 400 'hyprctl dispatch dpms off' \
-timeout 6000 'systemctl suspend' \
-resume 'hyprctl dispatch dpms on' \
-before-sleep 'swaylock'
+timeout 1200 'notify-send "Idle for 20 minutes... locking" && sleep 5 && swaylock'
+# timeout 400 'notify-send "Idle for 400 seconds... putting display to sleep" && sleep 5 && hyprctl dispatch dpms off' \
+# timeout 900 'systemctl suspend' 
+# resume 'hyprctl dispatch dpms on' 
+# before-sleep 'swaylock'
